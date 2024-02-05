9078f7ae23ec82205be97efc8e0c24b798d88b94
Federico Carabelli <fcarabelli@grupoprominente.com>
2024-02-05 10:47:55 -0300

    Siguiente commit

--- main.go
+++ main.go
@@ -9,12 +9,26 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/PortfolioCarabelli/godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	estado, text := variables.ConviertoaTexto(1588)
-	fmt.Println(estado)
-	fmt.Println(text)
+	// estado, text := variables.ConviertoaTexto(1588)
+	// fmt.Println(estado)
+	// fmt.Println(text)
+
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("No es linux", os)
+	} else {
+		fmt.Println("Es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("esto es linux")
+	case "windows":
+		fmt.Println("esto es Windows")
+	default:
+		fmt.Printf("%s \n ", os)
+	}
 }
